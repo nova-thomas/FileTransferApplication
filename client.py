9d63fe6706549c8433b624775922c20cd9931c59
Nova Thomas <97986758+nova-thomas@users.noreply.github.com>
2023-11-06 14:33:00 -0500

    Basic client implementation

--- client.py
+++ client.py
@@ -12,7 +12,29 @@ SERVER_DATA_PATH = "server_data"
 
 
 def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
+    while True:
+        data = client.recv(SIZE).decode(FORMAT)
+        cmd, msg = data.split("@")
+        if cmd == "OK":
+            print(f"{msg}")
+        elif cmd == "DISCONNECTED":
+            print(f"{msg}")
+            break
 
+        data = input("> ")
+        data = data.split(" ")
+        cmd = data[0]
+
+        if cmd == "":
+            client.send(cmd.encode(FORMAT))
+        elif cmd == "LOGOUT":
+            client.send(cmd.encode(FORMAT))
+            break
+
+        print("Disconnected from the server.")
+        client.close()
 
     if __name__ == "__main__":
         main()
