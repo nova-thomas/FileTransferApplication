2a89c2f776c08acee6d8c2f3431b7efcbf85938c
Nova Thomas <97986758+nova-thomas@users.noreply.github.com>
2023-11-06 14:27:25 -0500

    Created server and client files, set up basic server connection

--- client.py
+++ client.py
@@ -2,3 +2,17 @@
 
 import os
 import socket
+
+IP = "localhost"
+PORT = 4450
+ADDR = (IP,PORT)
+SIZE = 1024 ## byte .. buffer size
+FORMAT = "utf-8"
+SERVER_DATA_PATH = "server_data"
+
+
+def main():
+
+
+    if __name__ == "__main__":
+        main()
--- server.py
+++ server.py
@@ -1,5 +1,45 @@
 # Author : Nova Thomas
 
+from cgitb import handler
 import os
 import socket
 import threading
+
+IP = "localhost" ### gethostname()
+PORT = 4450
+ADDR = (IP,PORT)
+SIZE = 1024
+FORMAT = "utf-8"
+SERVER_PATH = "server"
+
+def handle_client (conn,addr):
+    print(f"NEW CONNECTION: {addr} connected.")
+    
+    while True:
+        data = conn.recv(SIZE).decode(FORMAT)
+        data = data.split("@")
+        cmd = data[0]
+        
+        send_data = "OK@"
+
+        if cmd == "LOGOUT":
+            break
+
+        
+
+    print(f"{addr} disconnected")
+    conn.close()
+
+def main():
+    print("Starting the server")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    server.listen()
+    print(f"server is listening on {IP}: {PORT}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target = handle_client, args = (conn, addr))
+        thread.start()
+
+    if __name__ == "__main__":
+        main()
